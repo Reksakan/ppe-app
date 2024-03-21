068c5760b2d2e951b0e2f7ff9f97846e88df3148
reksakan <reksakan@reksa-debian.debian>
2024-03-20 23:59:07 -0400

    one more try to fetch multiple data from bls.gov

--- client/src/scenes/home/index.jsx
+++ client/src/scenes/home/index.jsx
@@ -3,38 +3,48 @@ import React from 'react'
 const Home = () => {
   //following should be moved to backend. 
 
-  const URL = "https://api.bls.gov/publicAPI/v2/timeseries/data/";
+  // const URL = 'https://api.bls.gov/publicAPI/v2/timeseries/data/';
+  
+  // const headers = { 
+  //   'Content-Type': 'application/json',
+  // };
+  // const query = JSON.stringify({
+  //   seriesid: ['LEU0254555900', 'APU0000701111'],
+  //   startyear: '2002',
+  //   endyear: '2012',
+  // });
 
-  // const header = {
-  //   'Access-Control-Allow-Origin': '*',
-  //   'Content-type': 'application/json',
+  // const getBlsData = async () => {
+
+  //   try{
+  //     const response = await fetch(URL, {
+  //       method: 'POST',
+  //       mode: 'no-cors',
+  //       headers: headers,
+  //       data: query,
+  //       registrationkey: '4384cf67934742af9771cb1f352e1075',
+        
+  //     });
+  //     if(!response.ok){
+  //       throw new Error("Could not getch resource");
+  //     }
+
+  //     const output = await response.json();
+  //     console.log('output: ', output);
+  //   } 
+  //   catch(error){
+  //     console.log(error)
+  //   }
   // };
 
-  const headers = {
-    // 'Access-Control-Allow-Origin': '*',
-    'Content-type': 'application/json',
-  };
-
-  const payload = JSON.stringify({
-    "seriesid":["LAUCN040010000000005", "LAUCN040010000000006"],
-    "startyear":"2010", 
-    "endyear":"2012",
-    // catalog: true,
-    // calculatoins: true,
-    // annualaverage: true,
-    // aspects: true,
-    "registrationkey": '4384cf67934742af9771cb1f352e1075',
-  })
-
+  //Working variant
+  // const URL = "https://api.bls.gov/publicAPI/v2/timeseries/data/SUUR0000SA0/";
+  const URL = "https://api.bls.gov/publicAPI/v2/timeseries/data/LEU0254555900";
+  
   const getBlsData = async () => {
 
     try{
-      const response = await fetch(URL, {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: headers,
-        body: payload,
-      });
+      const response = await fetch(URL);
       if(!response.ok){
         throw new Error("Could not getch resource");
       }
